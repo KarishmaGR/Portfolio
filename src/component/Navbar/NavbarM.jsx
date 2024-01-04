eee5e58fba65256cf62803a6114d09d4cb07d074
KarishmaGR <karishmasvryadav@gmail.com>
2024-01-04 11:19:37 +0530

    url change

--- src/component/Navbar/NavbarM.jsx
+++ src/component/Navbar/NavbarM.jsx
@@ -75,7 +75,7 @@ const NavbarM = () => {
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-900 rounded-lg bg-gray-900 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
               <li>
                 <a
-                  href="about"
+                  href="#about"
                   className="block py-2 px-3 text-gray-100 rounded hover:text-purple-800"
                   aria-current="page"
                 >
